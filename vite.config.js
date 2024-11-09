547537878ca11d9c53b265908ae12ada15f528b3
viniersiren <devinstuddard@gmail.com>
2024-11-09 09:05:44 -0800

    test6

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    rollupOptions: {
-      input: './main.jsx',
-    },
-  },
-  resolve: {
-    extensions: ['.js', '.jsx'],
-  },
 })
-
